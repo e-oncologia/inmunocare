e1d891cdb24efd04383a75488e33531ca13f15e8
e-oncologia <52789797+e-oncologia@users.noreply.github.com>
2019-09-14 23:56:12 +0200

    14septiembreA

--- js/test.js
+++ js/test.js
@@ -1,58 +1,59 @@
 function mostrar() {
-            div = document.getElementById('flotante');
-            div.style.display = '';
-        }
+  div = document.getElementById("flotante1");
+  div.style.display = "";
+}
 
 function mostrarIncorrecto() {
-            div = document.getElementById('flotanteIncorrecto');
-            div.style.display = '';
-        }
-
+  div = document.getElementById("flotanteIncorrecto");
+  div.style.display = "";
+}
 
 function cerrar() {
-            div = document.getElementById('flotante');
-            div.style.display = 'none';
-        }
+  div = document.getElementById("flotante");
+  div.style.display = "none";
+}
 
 function cerrarIncorrecto() {
-            div = document.getElementById('flotanteIncorrecto');
-            div.style.display = 'none';
-        }
+  div = document.getElementById("flotanteIncorrecto");
+  div.style.display = "none";
+}
 
 function resultado() {
-        var p1;
+  var p1;
 
-        if (document.getElementById('p1').checked==true) {cerrarIncorrecto(), mostrar()}
-        else {cerrar(), mostrarIncorrecto()}
-        }
-
-
-
+  if (document.getElementById("a1").checked == true) {
+    cerrarIncorrecto(), mostrar();
+  } else {
+    cerrar(), mostrarIncorrecto();
+  }
+}
 
 function mostrarB() {
-        div = document.getElementById('flotanteB');
-        div.style.display = '';
-        }
+  div = document.getElementById("flotanteB");
+  div.style.display = "";
+}
 
 function mostrarIncorrectoB() {
-        div = document.getElementById('flotanteIncorrectoB');
-        div.style.display = '';
-        }
-
+  div = document.getElementById("flotanteIncorrectoB");
+  div.style.display = "";
+}
 
 function cerrarB() {
-        div = document.getElementById('flotanteB');
-        div.style.display = 'none';
-        }
+  div = document.getElementById("flotanteB");
+  div.style.display = "none";
+}
 
 function cerrarIncorrectoB() {
-        div = document.getElementById('flotanteIncorrectoB');
-        div.style.display = 'none';
-        }
+  div = document.getElementById("flotanteIncorrectoB");
+  div.style.display = "none";
+}
 
 function resultadoB() {
-        var b1;
+  var b1;
 
-        if (document.getElementById('b1').checked==true) {cerrarIncorrectoB(), mostrarB()}
-        else {cerrarB(), mostrarIncorrectoB()}
-        }
+  if (document.getElementById("b1").checked == true) {
+    cerrarIncorrectoB(), mostrarB();
+  } else {
+    cerrarB(), mostrarIncorrectoB();
+  }
+}
